b38f0a2ee7a45c9d615532b2e329c817e6ed9020
Linh Tran <52797453+thewind121212@users.noreply.github.com>
2024-12-23 18:47:42 +0700

    add join room modal and join room logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,21 @@ import React, { useEffect } from 'react'
 import Caro from './components/Caro'
 import { BarLoader } from 'react-spinners'
 import { axiosClient } from './utils/axiosClient'
-import CreateTableModal , { CreateName } from './components/Modal'
+import CreateTableModal, { CreateName, JoinTable } from './components/Modal'
 import { useGameStore } from './store/game'
 import { useNavigate, useParams } from 'react-router-dom'
 
 
 export function CreateTableView() {
   return (
-    <div className='w-svw h-svh pl-[80px] flex justify-center items-center bg-[#212F40]'>
-      <CreateTableModal />
+    <div className='w-svw h-svh pl-[80px] flex justify-center flex-col items-center bg-[#212F40]'>
+
+      <h1 className='text-5xl text-white font-semibold mb-6'>Welcome to Caro Game!</h1>
+      <p className='text-xl mb-10 text-slate-400'>Challenge your friends and have fun. Create your room to start!</p>
+      <div className="flex justify-center items-center gap-4">
+        <JoinTable isShowModal={false} />
+        <CreateTableModal />
+      </div>
     </div>
   )
 }
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -82,8 +82,6 @@ export default function CreateTable() {
                 </animated.div>
             }
             <div className="flex flex-col items-center justify-center ">
-                <h1 className='text-5xl text-white font-semibold mb-6'>Welcome to Caro Game!</h1>
-                <p className='text-xl mb-10 text-slate-400'>Challenge your friends and have fun. Create your room to start!</p>
                 <Button onClickEventHandler={onClickEventHandler} content='Create Room' />
             </div>
         </>
@@ -91,7 +89,7 @@ export default function CreateTable() {
 }
 
 
-export function CreateName({isShowModal = true} : {isShowModal: boolean}) {
+export function CreateName({ isShowModal = true }: { isShowModal: boolean }) {
 
     const [showModal, setShowModal] = useState<boolean>(isShowModal)
     const { setPlayerName, setPlayerId, playerName, playerId } = useGameStore()
@@ -146,6 +144,76 @@ export function CreateName({isShowModal = true} : {isShowModal: boolean}) {
                     </div>
                 </div>
             </animated.div>
+        </>
+    )
+
+}
+
+export function JoinTable({ isShowModal = true }: { isShowModal: boolean }) {
+
+    const [showModal, setShowModal] = useState<boolean>(isShowModal)
+    const [roomInput, setRoomInput] = useState<string>('')
+    const navigate = useNavigate()
+
+
+
+    const showModalAnimation = useSpring({
+        opacity: showModal ? 1 : 0,
+        from: {
+            opacity: 0,
+        },
+        config: {
+            duration: 200
+        },
+        delay: 100
+    })
+
+    const joinGame = async () => {
+        if (roomInput === '') {
+            setShowModal(false)
+            return
+        }
+        setShowModal(false)
+        navigate(`/caro/${roomInput}`)
+    }
+
+    return (
+        <>
+            {
+                showModal && (
+                    <animated.div className="flex fixed justify-center items-center w-svw h-svh pl-[80px] z-[999] left-0 top-0" style={showModalAnimation}>
+                        <div className="wraper w-full h-full flex justify-center items-center relative">
+                            <div className="absolute w-full h-full bg-[#9898981b] z-10"
+                                onClick={() => setShowModal(false)}
+                            ></div>
+                            <div className="w-full max-w-[482px] h-auto bg-white relative z-20 flex justify-center items-center flex-col rounded-[3px] overflow-hidden">
+                                <div className="form-header bg-[#0C141D] w-full h-auto p-4 relative flex justify-center items-center">
+                                    <h1 className='text-white text-lg font-bold'>Join Game</h1>
+                                    <IoClose className='absolute top-1/2 -translate-y-1/2 right-2 text-white cursor-pointer' size={28} onClick={() => setShowModal(false)} />
+                                </div>
+                                <div className="w-full h-auto bg-[#101C26] flex justify-center items-center py-6">
+                                    <form className='flex flex-col gap-4'>
+                                        <div className="flex flex-col gap-2">
+                                            <input type="text" placeholder='Enter Room Id' className='p-4 bg-[#1B2838] text-white border-transparent text-3xl text-center  rounded-md outline-none' id='name'
+                                                onChange={(e) => setRoomInput(e.target.value)}
+                                                value={roomInput}
+                                            />
+                                        </div>
+                                    </form>
+                                </div>
+                                <div className="form-header bg-[#0C141D] w-full h-auto p-4 relative flex justify-end">
+                                    <Button onClickEventHandler={joinGame} className='bg-[#18BC9C] w-fit' content='Join Room' />
+                                </div>
+                            </div>
+                        </div>
+                    </animated.div>
+
+                )
+            }
+
+            <div className="flex flex-col items-center justify-center ">
+                <Button onClickEventHandler={() => setShowModal(true)} content='Join Room' className='bg-red-400' />
+            </div>
         </>
     )
 
