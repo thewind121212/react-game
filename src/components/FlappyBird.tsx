e5b13b3cf1735347056d35f0ca6868b20ed6e530
Linh Tran <52797453+thewind121212@users.noreply.github.com>
2024-12-31 16:56:59 +0700

    impove the game and limit the frame rate at 60fps

--- src/components/FlappyBird.tsx
+++ src/components/FlappyBird.tsx
@@ -1,21 +1,21 @@
 import React, { useEffect, useRef, useCallback } from "react";
 
+const BOARDWIDTH = 360;
+const BOARDHEIGHT = 640;
 const BIRDWIDTH = 34;
 const BIRDHEIGHT = 24;
 const BIRDSTARTPX = 360 / 8;
 const BIRDSTARTPY = 640 / 2;
 const birdImgFrame: HTMLImageElement[] = [];
-const BIRDFLYVELOCITY = 3;
+const BIRDFLYVELOCITY = 6;
 const BIRDFLYACCELERATION = 0.5;
-const g = (2 / 200 * 60);
-
+const GRAVITY_CONSTANT = (15 / 200 * 60);
 const PIPEWIDTH = 64;
 const INITPIPEHEIGHT = 512;
-
 let pipeImgTop: HTMLImageElement;
 let pipeImgBottom: HTMLImageElement;
-
-const flapInterval = 100;
+const FLAPINTERVAL = 100;
+const FRAME_ROTATION_INTENSITY = 1.8;
 
 const genPipe = () => {
   const positonTop = Math.floor(Math.random() * (480 - 110 + 1)) + 110;
@@ -28,6 +28,7 @@ const genPipe = () => {
     ybottomPipe: 640 - positonTop + gapRandom,
   };
 };
+
 
 export default function FlappyBird() {
   const context = useRef<CanvasRenderingContext2D | null>(null);
@@ -50,21 +51,33 @@ export default function FlappyBird() {
   const animationFrameRef = useRef<number>(0);
   const keyStrokeTimer = useRef<ReturnType<typeof setTimeout> | null>(null);
   const pipeArray = useRef<{ xtopPipe: number; ytopPipe: number; xbottomPipe: number; ybottomPipe: number }[]>([]);
-  const pipeIntervalRender = useRef<number>(2000);
   const accelerationTime = useRef<number>(0);
+  const disablePress = useRef<boolean>(false);
+  const frameTime = useRef<number>(0);
+  const pipeRenderInterval = useRef<ReturnType<typeof setInterval> | null>(0);
 
-  const lastRenderPipeTime = useRef<number>(0);
 
   const updateByFrame = (timestamp: number) => {
+    // Limit frame rate at 60fps
+    const deltaFrameT = timestamp - frameTime.current;
+    if (deltaFrameT < 1000 / 60) {
+      animationFrameRef.current = requestAnimationFrame(updateByFrame);
+      return;
+    }
+    frameTime.current = timestamp;
+
+
+    // Calculate delta time for flappy wings
     const deltaT = (timestamp - lastFrameTime.current) / 1000;
     let isCollide = false;
-    birdVelocityByG.current += g * deltaT;
+    birdVelocityByG.current += GRAVITY_CONSTANT * deltaT;
 
     birdRef.current.y += birdVelocityByG.current;
 
     if (birdRef.current.rotation <= 90) {
-      birdRef.current.rotation += 1;
+      birdRef.current.rotation += FRAME_ROTATION_INTENSITY;
     }
+
 
     if (birdRef.current.y > 510) {
       isCollide = true;
@@ -81,7 +94,7 @@ export default function FlappyBird() {
         Yb < pipe.ytopPipe + INITPIPEHEIGHT &&
         Xb < pipe.xtopPipe + PIPEWIDTH - 4
       ) {
-        isCollide = true;
+        disablePress.current = true;
       }
 
       if (
@@ -89,7 +102,7 @@ export default function FlappyBird() {
         Yb > pipe.ybottomPipe - BIRDHEIGHT &&
         Xb < pipe.xbottomPipe + PIPEWIDTH - 4
       ) {
-        isCollide = true;
+        disablePress.current = true;
       }
     });
 
@@ -98,29 +111,26 @@ export default function FlappyBird() {
       return;
     }
 
-    if (timestamp - lastFrameTime.current > flapInterval) {
+    if (timestamp - lastFrameTime.current > FLAPINTERVAL) {
       birdFrame.current = (birdFrame.current + 1) % 3;
       lastFrameTime.current = timestamp;
     }
 
-    if (timestamp - lastRenderPipeTime.current > pipeIntervalRender.current) {
-      const { xtopPipe, ytopPipe, xbottomPipe, ybottomPipe } = genPipe();
-      pipeArray.current.push({ xtopPipe, ytopPipe, xbottomPipe, ybottomPipe });
-      pipeIntervalRender.current = Math.floor(Math.random() * 1000) + 1500;
-      lastRenderPipeTime.current = timestamp;
-    }
 
     if (context.current) {
       context.current.clearRect(0, 0, 360, 640);
 
       // Draw pipes
       pipeArray.current.forEach((pipe, index) => {
-        pipe.xtopPipe -= 1;
-        pipe.xbottomPipe -= 1;
+        if (!disablePress.current) {
+          pipe.xtopPipe -= 4;
+          pipe.xbottomPipe -= 4;
+        }
         if (context.current) {
           context.current.drawImage(pipeImgTop, pipe.xtopPipe, pipe.ytopPipe, PIPEWIDTH, INITPIPEHEIGHT);
           context.current.drawImage(pipeImgBottom, pipe.xbottomPipe, pipe.ybottomPipe, PIPEWIDTH, INITPIPEHEIGHT);
         }
+
 
         if (pipe.xtopPipe + PIPEWIDTH < -360) {
           pipeArray.current.splice(index, 1);
@@ -141,10 +151,13 @@ export default function FlappyBird() {
       context.current.restore();
     }
 
+
+
     animationFrameRef.current = requestAnimationFrame(updateByFrame);
   };
 
   const onPressSpace = useCallback(() => {
+    if (disablePress.current) return
     if (birdRef.current.y + 45 < 0) {
       if (keyStrokeTimer.current) {
         clearTimeout(keyStrokeTimer.current);
@@ -166,27 +179,30 @@ export default function FlappyBird() {
   useEffect(() => {
     const board = document.getElementById("flappybird-board") as HTMLCanvasElement;
     if (board) {
-      board.width = 360;
-      board.height = 640;
+      board.width = BOARDWIDTH;
+      board.height = BOARDHEIGHT;
       context.current = board.getContext("2d");
 
+
+
+      // Load images for bird and pipes
       for (let i = 0; i < 3; i++) {
         birdImgFrame[i] = new Image();
         birdImgFrame[i].src = `/src/assets/flappybird/birdFrame${i + 1}.png`;
       }
 
-      // Load pipe images
+
       pipeImgTop = new Image();
       pipeImgTop.src = '/src/assets/flappybird/top-pipe.png';
       pipeImgBottom = new Image();
       pipeImgBottom.src = '/src/assets/flappybird/bottom-pipe.png';
 
-      // Generate initial pipe
+      animationFrameRef.current = requestAnimationFrame(updateByFrame);
+
+      //init the first pipe
       const { xtopPipe, ytopPipe, xbottomPipe, ybottomPipe } = genPipe();
       pipeArray.current.push({ xtopPipe, ytopPipe, xbottomPipe, ybottomPipe });
 
-      // Start animation loop
-      animationFrameRef.current = requestAnimationFrame(updateByFrame);
     }
 
     return () => {
@@ -197,25 +213,34 @@ export default function FlappyBird() {
 
   useEffect(() => {
     document.addEventListener("keydown", (e) => {
-      if (e.code === "Space") {
+      if (e.code === "Space" || e.code === "ArrowUp" || e.code === "KeyW") {
         onPressSpace();
       }
     });
 
+
+    pipeRenderInterval.current = setInterval(() => {
+      const { xtopPipe, ytopPipe, xbottomPipe, ybottomPipe } = genPipe();
+      pipeArray.current.push({ xtopPipe, ytopPipe, xbottomPipe, ybottomPipe });
+    }, 1400)
+
     return () => {
       document.removeEventListener("keydown", onPressSpace);
+      clearInterval(pipeRenderInterval.current!);
     };
   }, [onPressSpace]);
 
   return (
     <div className="w-svw h-svh pl-[80px] flex justify-center items-center bg-[#212F40]">
-      <div className="w-[360px] h-[640px] bg-white relative">
+      <div className="w-[360px] h-[640px] bg-white relative"
+        onClick={onPressSpace}
+      >
         <canvas
           className="w-full h-full absolute top-0 left-0 z-10"
           id="flappybird-board"
         ></canvas>
-        <div className="w-full h-auto absolute bottom-0 left-0 z-20">
-          <img src="/src/assets/bottom-background.png" alt="background-bt" className="w-full h-auto" />
+        <div className="w-full h-auto absolute bottom-0 left-0 z-20 pointer-events-none">
+          <img src="/src/assets/bottom-background.png" alt="background-bt" className="w-full h-auto pointer-events-none" />
         </div>
       </div>
     </div>
